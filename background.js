c3d4f380f80fa0c46930a359336a35ba2997bb1c
Victor J Wang <vwang@flatiron.com>
2019-06-30 17:16:10 -0400

    fix typo

--- background.js
+++ background.js
@@ -1,16 +1,14 @@
-
-
-chrome.runtime.onInstalled.addListene3r(function() {
+chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("the color is green");
+    console.log('The color is green.');
   });
-
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStageMatcher({
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]),
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
