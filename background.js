8bcf48f1bd6d33a5231bcd846ade07b3d0e7589d
Victor J Wang <vwang@flatiron.com>
2019-06-30 16:49:41 -0400

    init extension example

---
+++ background.js
@@ -0,0 +1,17 @@
+
+
+chrome.runtime.onInstalled.addListene3r(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("the color is green");
+  });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStageMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]),
+});
+
